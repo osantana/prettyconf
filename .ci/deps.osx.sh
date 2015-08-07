4c644ac867d7a90202920da28192cdd11da680b2
Erick Wilder <erickwilder@gmail.com>
2015-08-07 09:55:49 -0300

    Change Travis configuration for MacOSX
    - Call pyenv rehash after installation
    - Reorder PATH definition

--- .ci/deps.osx.sh
+++ .ci/deps.osx.sh
@@ -8,4 +8,5 @@ brew outdated pyenv || brew upgrade --quiet pyenv
 # Install required python version for this build
 pyenv install -ks $PYTHON_VERSION
 pyenv global $PYTHON_VERSION
+pyenv rehash
 python --version
