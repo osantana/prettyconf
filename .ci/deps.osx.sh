c144a2015cde84c13bbde11a303c215cafede741
Hernan Lozano <hernantz@gmail.com>
2019-04-03 14:55:25 -0300

    Removed --quiet arg since it's breaking the build

--- .ci/deps.osx.sh
+++ .ci/deps.osx.sh
@@ -3,7 +3,7 @@ set -x
 
 # Install packages with brew
 brew update >/dev/null
-brew outdated pyenv || brew upgrade --quiet pyenv
+brew outdated pyenv || brew upgrade pyenv
 
 # Install required python version for this build
 pyenv install -ks $PYTHON_VERSION
