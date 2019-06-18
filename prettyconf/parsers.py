c6a937106617c1918c41391f42185ea96094413b
Jayson Reis <santosdosreis@gmail.com>
2019-06-18 10:23:31 +0200

    Break EnvFileParser parser logic into smaller functions

--- prettyconf/parsers.py
+++ prettyconf/parsers.py
@@ -1,9 +1,13 @@
+from typing import Union, Tuple, Iterator
+
 STATE_INITIAL = "initial"
 STATE_PARSING_KEY = "parsing_key"
 STATE_PARSING_VALUE = "parsing_value"
 STATE_PARSING_VALUE_ESCAPE = "parsing_value_escape"
 STATE_PARSING_COMMENT = "parsing_comment"
 
+COMMENT = "#"
+END_OF_LINE = "\n"
 ESCAPE_CHAR = "\\"
 SPACES = set(" \n")
 QUOTES = set("'\"")
@@ -34,7 +38,7 @@ class EnvFileParser:
         self.position += 1
         return char
 
-    def parse_config(self):
+    def parse_config(self) -> Iterator[Tuple[str, str]]:
         key = self._current_key
         value = self._current_value
 
@@ -45,75 +49,127 @@ class EnvFileParser:
                     yield self._return_current_config()
                 break
 
-            if self.state == STATE_INITIAL:
-                if char == "#":
-                    self.state = STATE_PARSING_COMMENT
-                    continue
-                if char in SPACES:
-                    continue
-                key.append(char)
-                self.state = STATE_PARSING_KEY
+            if self._process_initial(char):
                 continue
 
-            if char in QUOTES:
-                if not self._current_quote:
-                    self._current_quote = char
-                    continue
-                if self._current_quote == char:
-                    self._current_quote = ""
-                    continue
-
-                value.append(char)
+            if self._process_quotes(char):
                 continue
 
-            if char == "#" and not self._current_quote:
-                self.state = STATE_PARSING_COMMENT
+            if self._start_comment(char):
                 continue
 
-            if self.state == STATE_PARSING_COMMENT and char == "\n":
-                if not (key or value) or not self._key_parsed:
-                    # Key started with a comment
-                    self._reset_state()
-                    continue
-                yield self._return_current_config()
+            parsed_value = self._finish_comment(char)
+            if parsed_value:
+                if isinstance(parsed_value, tuple):
+                    yield parsed_value
                 continue
 
-            if self.state == STATE_PARSING_KEY:
-                if char == "=":
-                    self.state = STATE_PARSING_VALUE
-                    self._key_parsed = True
-                    continue
-                if char == "\n":
-                    self._reset_state()
-                    continue
-                key.append(char)
+            if self._parse_key(char):
                 continue
 
-            if self.state == STATE_PARSING_VALUE:
-                if char == ESCAPE_CHAR:
-                    self.state = STATE_PARSING_VALUE_ESCAPE
-                    continue
+            parsed_value = self._parse_value(char)
+            if parsed_value:
+                if isinstance(parsed_value, tuple):
+                    yield parsed_value
+                continue
 
-                if char == " " and not (value or self._current_quote):
-                    continue
+            if self._parse_escaped_value(char):
+                continue
 
-                if char == "\n":
-                    yield self._return_current_config()
-                    continue
+    def _start_comment(self, char):
+        if char == COMMENT and (not self._current_quote or self.state == STATE_INITIAL):
+            self.state = STATE_PARSING_COMMENT
+            return True
 
-                value.append(char)
+        return False
 
-            if self.state == STATE_PARSING_VALUE_ESCAPE:
-                if char == "\n":
-                    self.state = STATE_PARSING_VALUE
-                    continue
-                else:
-                    # That was a literal \
-                    self.state = STATE_PARSING_VALUE
-                    value.extend([ESCAPE_CHAR, char])
-                    continue
+    def _finish_comment(self, char) -> Union[bool, Tuple[str, str]]:
+        if not (self.state == STATE_PARSING_COMMENT and char == END_OF_LINE):
+            return False
 
-    def _return_current_config(self):
+        if not (self._current_key or self._current_value) or not self._key_parsed:
+            # Key started with a comment
+            self._reset_state()
+            return True
+
+        return self._return_current_config()
+
+    def _process_quotes(self, char):
+        if char not in QUOTES:
+            return False
+        if not self._current_quote:
+            self._current_quote = char
+            return True
+
+        if self._current_quote == char:
+            self._current_quote = ""
+            return True
+
+        self._current_value.append(char)
+        return True
+
+    def _process_initial(self, char):
+        if self.state != STATE_INITIAL:
+            return False
+
+        if self._start_comment(char):
+            return True
+
+        if char in SPACES:
+            return True
+
+        self._current_key.append(char)
+        self.state = STATE_PARSING_KEY
+        return True
+
+    def _parse_key(self, char):
+        if self.state != STATE_PARSING_KEY:
+            return False
+
+        if char == "=":
+            self.state = STATE_PARSING_VALUE
+            self._key_parsed = True
+            return True
+
+        if char == END_OF_LINE:
+            self._reset_state()
+            return True
+
+        self._current_key.append(char)
+        return True
+
+    def _parse_value(self, char) -> Union[bool, Tuple[str, str]]:
+        if self.state != STATE_PARSING_VALUE:
+            return False
+
+        if char == ESCAPE_CHAR:
+            self.state = STATE_PARSING_VALUE_ESCAPE
+            return True
+
+        if char == " " and not (self._current_value or self._current_quote):
+            return True
+
+        if char == END_OF_LINE:
+            return self._return_current_config()
+
+        self._current_value.append(char)
+        return True
+
+    def _parse_escaped_value(self, char):
+        if self.state != STATE_PARSING_VALUE_ESCAPE:
+            return False
+
+        if char == END_OF_LINE:
+            # resume value parsing on the next line
+            self.state = STATE_PARSING_VALUE
+            return True
+
+        # That was a literal \
+        self.state = STATE_PARSING_VALUE
+        self._current_value.extend([ESCAPE_CHAR, char])
+        return True
+
+    def _return_current_config(self) -> Tuple[str, str]:
         key, value = "".join(self._current_key), "".join(self._current_value)
         self._reset_state()
         return key.rstrip(), value.rstrip()
