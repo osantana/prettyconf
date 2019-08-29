50c2bb207ef8c9d7b7539458bd402b7866778e44
Osvaldo Santana Neto <osantana@osantana.me>
2019-08-29 15:15:51 +0200

    Convert Configuration tests to pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,17 @@ import pytest
 from prettyconf.loaders import CommandLine
 from .factory import parser_factory
 
+ENVFILE_CONTENT = """
+ENVFILE=Environment File Value
+ENVVAR=Must be overrided
+"""
+
+INIFILE_CONTENT = """
+[settings]
+INIFILE=INI File Value
+ENVFILE=Must be overrided
+"""
+
 
 @pytest.fixture
 def files_path():
@@ -26,3 +37,28 @@ def command_line_config():
     parser = parser_factory()
     parser.parse_args = parse_args
     return CommandLine(parser=parser)
+
+
+def _create_file(filename, content):
+    with open(filename, "a") as file_:
+        file_.write(content)
+
+
+@pytest.fixture
+def env_config(files_path):
+    envfile = files_path + "/../.env"
+    _create_file(envfile, ENVFILE_CONTENT)
+
+    yield envfile
+
+    os.remove(envfile)
+
+
+@pytest.fixture
+def ini_config(files_path):
+    inifile = files_path + "/../settings.ini"
+    _create_file(inifile, INIFILE_CONTENT)
+
+    yield inifile
+
+    os.remove(inifile)
